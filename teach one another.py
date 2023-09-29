d8d35098aa47190ad00a0599ecda5c621d8737be
GaoYingZhang <145675909+GaoYingZhang@users.noreply.github.com>
2023-09-29 17:18:48 +0700

    Add files via upload

---
+++ teach one another.py
@@ -0,0 +1,13 @@
+noun1=input("my history")
+noun2=input("school")
+Verb=input("active")
+noun3=input("classmates")
+adjective=input("good")
+
+# print madlib my history
+#  madlib=("When l studyed in +"noun2"+,many my +"noun3"+,they like to +"verb"+ in sporting place, They were all +"adjective"+students.that
+# was +"noun1").
+ 
+
+
+
